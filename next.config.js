c69b579fc9f7af2fb4aa75eccd5d09a1a890cab1
Geesath Kannangara <skullmc101@gmail.com>
2024-11-29 02:22:19 +0530

    smth

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // ... other config options
-    
-    // Disable Speed Insights
-    speedInsights: {
-      enabled: false,
-    },
 
-    // Disable development mode indicator
     devIndicators: {
       buildActivity: false,
       buildActivityPosition: 'bottom-right',
